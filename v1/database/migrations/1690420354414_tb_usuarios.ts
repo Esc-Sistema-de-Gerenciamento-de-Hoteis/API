e5150c81707a6579f66c2f229458a49f4d00ebc8
Caio da Silveira Rodrigues <cdsr073@gmail.com>
2023-07-26 23:47:10 -0300

    Atualizando Migration

--- v1/database/migrations/1690420354414_tb_usuarios.ts
+++ v1/database/migrations/1690420354414_tb_usuarios.ts
@@ -6,12 +6,14 @@ export default class extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
+      table.string('email')
       table.string('login')
       table.string('nome')
       table.string('perfil')
       table.string('senha')
       table.string('sub')
       table.string('picture')
+      table.string('token')
       table.timestamp('created_at', { useTz: true })
       table.string('updated_at')
     })
